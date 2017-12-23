07faacd68d30ba3db3df20e0879f0c8972bfa79e
beny <jlbeny100@gmail.com>
2017-12-23 20:13:37 +0100

    feat : update junit dependency to 4.4 - and adding StringHelper class

---
+++ src/main/java/com/valoyes/projectjunit/helper/StringHelper.java
@@ -0,0 +1,36 @@
+package com.valoyes.projectjunit.helper;
+
+public class StringHelper {
+
+	/**
+	 * e.g : AACC => CD
+	 * e.g : ACD => CD
+	 * e.g : CDEF => CDEF
+	 * e.g : cdaa => CDAA 
+	 * @param str
+	 * @return
+	 */
+	public String truncateAInFirst2Positions(String str) {
+		if (str.length() <= 2)
+			return str.replaceAll("A", "");
+
+		String first2Chars = str.substring(0, 2);
+		String stringMinusFirst2Chars = str.substring(2);
+
+		return first2Chars.replaceAll("A", "") + stringMinusFirst2Chars;
+	}
+	
+	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+
+		if (str.length() <= 1)
+			return false;
+		if (str.length() == 2)
+			return true;
+
+		String first2Chars = str.substring(0, 2);
+
+		String last2Chars = str.substring(str.length() - 2);
+
+		return first2Chars.equals(last2Chars);
+	}
+}
