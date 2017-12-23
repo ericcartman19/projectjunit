b2c3471919f3c2eccf1c942f41becb27b0289557
beny <jlbeny100@gmail.com>
2017-12-23 23:10:10 +0100

    feat : added more TUs for StringHelper

--- src/test/java/com/valoyes/projectjunit/helper/StringHelperTest.java
+++ src/test/java/com/valoyes/projectjunit/helper/StringHelperTest.java
@@ -16,43 +16,57 @@ import org.junit.Test;
  */
 public class StringHelperTest {
 
-	private static final String ABCD_STRING_CONSTANT = "ABCD";
-	private static final String CDAA_STRING_CONSTANT = "CDAA";
-	private static final String CDEF_STRING_CONSTANT = "CDEF";
-	private static final String CD_STRING_CONSTANT = "CD";
+	private static final String A = "A";
+	private static final String AB = "AB";
+	private static final String ABAB = "ABAB";
+	private static final String ABCD = "ABCD";
+	private static final String ACD = "ACD";
+	private static final String AACD = "AACD";
+	private static final String CDAA = "CDAA";
+	private static final String CDEF = "CDEF";
+	private static final String CD = "CD";
 	StringHelper helper = new StringHelper();
 	
 	@Test
 	public void casPassant_truncateAInFirst2Positions_Ainfirst2Positions() {
-		assertEquals(CD_STRING_CONSTANT, helper.truncateAInFirst2Positions("AACD"));
+		assertEquals(CD, helper.truncateAInFirst2Positions(AACD));
 	}
 	
 	@Test
 	public void casPassant_truncateAInFirst2Positions_AinfirstPositionOnly() {
-		assertEquals(CD_STRING_CONSTANT, helper.truncateAInFirst2Positions("ACD"));
+		assertEquals(CD, helper.truncateAInFirst2Positions(ACD));
 	}
 	
 	@Test
 	public void casPassant_truncateAInFirst2Positions_NoAFoundInString() {
-		assertEquals(CDEF_STRING_CONSTANT, helper.truncateAInFirst2Positions("CDEF"));
+		assertEquals(CDEF, helper.truncateAInFirst2Positions(CDEF));
 	}
 	
 	@Test
 	public void casPassant_truncateAInFirst2Positions_AIn2LastPositions() {
-		assertEquals(CDAA_STRING_CONSTANT, helper.truncateAInFirst2Positions("CDAA"));
+		assertEquals(CDAA, helper.truncateAInFirst2Positions(CDAA));
 	}
 	
 	@Test
-	public void cassPassant_areFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+	public void casNonPassant_areFirstAndLastTwoCharactersTheSame_FourDifferentCharacters() {
 		// ABCD => false
-		assertFalse(helper.areFirstAndLastTwoCharactersTheSame(ABCD_STRING_CONSTANT));
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame(ABCD));
 	}
 	
 	@Test
 	public void casPassant_areFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
-		// ABAB => true
+		assert(helper.areFirstAndLastTwoCharactersTheSame(ABAB));
 	}
+	
+	@Test
+	public void casPassant_areFirstAndLastTwoCharactersTheSame_TwoCharactersOnly() {
 		// AB => true
-		// A = false
-
+		assert(helper.areFirstAndLastTwoCharactersTheSame(AB));
+	}
+	
+	@Test
+	public void casNonPassant_areFirstAndLastTwoCharactersTheSame_OnlyOneCharacter() {
+		// A => false
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame(A));
+	}
 }
