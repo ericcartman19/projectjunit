f78ce2307a65ffaf469463422d86cb746f8ecede
beny <jlbeny100@gmail.com>
2017-12-24 01:26:46 +0100

    feat : add sample on how to handle exceptions in junit - using expected in @Test(expected=NPE.class)

--- src/test/java/com/valoyes/projectjunit/helper/ArrayCompareTest.java
+++ src/test/java/com/valoyes/projectjunit/helper/ArrayCompareTest.java
@@ -10,14 +10,38 @@ public class ArrayCompareTest {
 
 	@Test
 	public void casPassant_ComparingTwoArrays() {
-		
+
 		int[] numbers = {12, 3, 4, 1};
 		// como deberia ser el array, una vez sorted
 		int[] expected = {1, 3, 4, 12};
 		Arrays.sort(numbers);
 		// assert
 		assertArrayEquals(expected, numbers);
+	}
+
+	@Test
+	public void testArraySort_NullArray_UsingTryCatch() {
+		// hacemos el array nulo
+		int[] numbers = null;
+		// lo intentamos ordenar
+		// esto es un escenario posible
+		// y lo que ocurre es que Arrays.sort lanza una 
+		// NPE como es de esperar
 		
+		try {
+		
+			Arrays.sort(numbers);
+		
+		}catch(NullPointerException e) {
+			// SUCCESS
+			// pq es lo esperado
+		}
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testArraySortNullArray_UsingParameterExpectedInAnnotation() {
+		int[] numbers = null;
+		Arrays.sort(numbers);
 	}
 
 }
